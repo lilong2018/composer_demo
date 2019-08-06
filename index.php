5b43b6210cc89b111f20305e8ed9ed8c91a6dc4e
lilong <571914591@qq.com>
2019-08-06 19:56:37 +0800

    composer示例

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 编辑器：PhpStorm.
+ * 编写者: 莉龙
+ * 编写时间: 2019/8/6 19:39
+ */
+
+require __DIR__ . '/vendor/autoload.php';
+
+use lilong\Say;
+
+echo Say::say();
+
---
+++ lilong/Say.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 编辑器：PhpStorm.
+ * 编写者: 莉龙
+ * 编写时间: 2019/8/6 19:42
+ */
+
+namespace lilong;
+
+class Say {
+    public static function say() {
+        return "5201314@YangLi";
+    }
+}
+
